5a6bb6197afad8ca3ee78e1c4c701dd0f1b5cf54
Anselm Spoerri <aspoerri@rutgers.edu>
2020-04-28 16:10:48 -0400

    First commit

---
+++ src/build/precache-manifest.0a3f93209358a10dde09e0beee467daa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e61885ae742c0b8f2a280fd94b536b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd36b1b242053a66e885",
+    "url": "/static/css/main.fe6824ef.chunk.css"
+  },
+  {
+    "revision": "e9b31015f404169a4f93",
+    "url": "/static/js/2.e741812d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e741812d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd36b1b242053a66e885",
+    "url": "/static/js/main.c12087c7.chunk.js"
+  },
+  {
+    "revision": "e988c064c70788dbc50e",
+    "url": "/static/js/runtime-main.a56884a5.js"
+  }
+]);
